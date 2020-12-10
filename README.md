777e69257628fc76a8ff484c1b539b3c71f57347
Thibaut <thibaut@etienne.pw>
2020-12-10 15:17:14 +0100

    Create project (#1)

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
+<p align=center>
+    <img src="https://upload.wikimedia.org/wikipedia/fi/thumb/2/2a/Veolia-logo.svg/250px-Veolia-logo.svg.png"/>
+</p>
 
+<p align=center>
+    <a href="https://pypi.org/project/pyolia/"><img src="https://img.shields.io/pypi/v/pyolia.svg"/></a>
+    <a href="https://github.com/tetienne/python-veolia-api/actions"><img src="https://github.com/tetienne/python-veolia-api/workflows/CI/badge.svg"/></a>
+    <a href="https://github.com/psf/black"><img src="https://img.shields.io/badge/code%20style-black-000000.svg" /></a>
+</p>
+
+Small client to retrieve the water consumption from Veolia website: https://www.eau-services.com
+## Installation
+
+```bash
+pip install pyolia
+```
+
+## Getting started
+
+```python
+import asyncio
+
+from pyolia.client import VeoliaClient
+
+USERNAME = "your username"
+PASSWORD = "your password"
+
+async def main() -> None:
+    async with VeoliaClient(USERNAME, PASSWORD) as client:
+        print(await client.get_consumption(12, 2020))
+
+
+asyncio.run(main())
+```
+
+## Development
+
+### Installation
+
+- For Linux, install [pyenv](https://github.com/pyenv/pyenv) using [pyenv-installer](https://github.com/pyenv/pyenv-installer)
+- For MacOS, run `brew install pyenv`
+- Don't forget to update your `.bashrc` file (or similar):
+  ```
+  export PATH="~/.pyenv/bin:$PATH"
+  eval "$(pyenv init -)"
+  ```
+- Install the required [dependencies](https://github.com/pyenv/pyenv/wiki#suggested-build-environment)
+- Install [poetry](https://python-poetry.org): `curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python`
+
+- Clone this repository
+- `cd python-veolia-api`
+- Install the required Python version: `pyenv install`
+- Init the project: `poetry install`
+- Run `poetry run pre-commit install`
+
+## PyCharm
+
+As IDE you can use [PyCharm](https://www.jetbrains.com/pycharm/).
+
+Using snap, run `snap install pycharm --classic` to install it.
+
+For MacOS, run `brew cask install pycharm-ce`
+
+Once launched, don't create a new project, but open an existing one and select the **python-veolia-api** folder.
+
+Go to _File | Settings | Project: nre-tag | Project Interpreter_. Your interpreter must look like `<whatever>/python-veolia-api/.venv/bin/python`
---
+++ pyolia/client.py
@@ -0,0 +1,92 @@
+""" Python wrapper for the Veolia unofficial API """
+from __future__ import annotations
+
+import csv
+from datetime import datetime
+from types import TracebackType
+from typing import Any, Dict, List, Optional, Type, Union
+
+import backoff
+from aiohttp import ClientSession
+
+JSON = Union[Dict[str, Any], List[Dict[str, Any]]]
+
+DOMAIN = "https://www.eau-services.com"
+LOGIN_URL = f"{DOMAIN}/default.aspx"
+DATA_URL = f"{DOMAIN}/mon-espace-suivi-personnalise.aspx?ex=1&mm={{}}/{{}}"
+
+
+async def relogin(invocation: Dict[str, Any]) -> None:
+    await invocation["args"][0].login()
+
+
+class NotAuthenticatedException(Exception):
+    pass
+
+
+class BadCredentialsException(Exception):
+    pass
+
+
+class VeoliaClient:
+    """ Interface class for the Veolia unofficial API """
+
+    def __init__(
+        self, username: str, password: str, session: ClientSession = None,
+    ) -> None:
+        """
+        Constructor
+
+        :param username: the username for eau-services.com
+        :param password: the password for eau-services.com
+        :param session: optional ClientSession
+        """
+
+        self.username = username
+        self.password = password
+        self.session = session if session else ClientSession()
+
+    async def __aenter__(self) -> VeoliaClient:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> None:
+        await self.close()
+
+    async def close(self) -> None:
+        """Close the session."""
+        await self.session.close()
+
+    @backoff.on_exception(
+        backoff.expo, NotAuthenticatedException, max_tries=2, on_backoff=relogin,
+    )
+    async def get_consumption(self, month: int, year: int) -> Dict[float, int]:
+        """Return the water consumption for the given month and year in liter."""
+        if month > 12 or month < 1:
+            raise ValueError("month must be between 1 and 12 included.")
+        if year < 2001:
+            raise ValueError("year must be greater than 2000")
+
+        async with self.session.get(DATA_URL.format(month, year)) as response:
+            if response.url.name == "inscription.aspx":
+                raise NotAuthenticatedException
+            data = await response.text()
+
+        reader = csv.reader(data.splitlines(), delimiter=";")
+        next(reader)  # skip header line
+        return {
+            datetime.strptime(row[0], "%d/%m/%Y").timestamp(): int(row[1])
+            for row in reader
+        }
+
+    async def login(self) -> None:
+        """Log into the Veolia website."""
+        async with await self.session.post(
+            LOGIN_URL, data={"login": self.username, "pass": self.password}
+        ) as response:
+            if response.url.name == "connexion.aspx":
+                raise BadCredentialsException
