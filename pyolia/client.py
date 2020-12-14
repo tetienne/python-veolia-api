a476e1136008c2afe0cb8e7fc253af8200eb66ee
Thibaut <thibaut@etienne.pw>
2020-12-14 11:36:51 +0100

    Add hourly water consumption (#7)

--- pyolia/client.py
+++ pyolia/client.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 import csv
-from datetime import datetime
+from datetime import date, datetime, timedelta
 from types import TracebackType
 from typing import Any, Dict, List, Optional, Type, Union
 
@@ -14,6 +14,10 @@ JSON = Union[Dict[str, Any], List[Dict[str, Any]]]
 DOMAIN = "https://www.eau-services.com"
 LOGIN_URL = f"{DOMAIN}/default.aspx"
 DATA_URL = f"{DOMAIN}/mon-espace-suivi-personnalise.aspx?ex=1&mm={{}}/{{}}"
+DATA_URL_DAY = f"{DATA_URL}&d={{}}"
+
+CSV_DELIMITER = ";"
+CONSUMPTION_HEADER = "consommation(litre)"
 
 
 async def relogin(invocation: Dict[str, Any]) -> None:
@@ -64,30 +68,66 @@ class VeoliaClient:
         """Close the session."""
         await self.session.close()
 
+    @property
+    def last_report_date(self) -> date:
+        """Date since which reports have been published."""
+        return datetime.now().date() - timedelta(days=3)
+
     @backoff.on_exception(
         backoff.expo,
         NotAuthenticatedException,
         max_tries=2,
         on_backoff=relogin,
     )
-    async def get_consumption(self, month: int, year: int) -> Dict[float, int]:
-        """Return the water consumption for the given month and year in liter."""
-        if month > 12 or month < 1:
-            raise ValueError("month must be between 1 and 12 included.")
+    async def get_consumption(
+        self, month: int, year: int, day: int = None
+    ) -> List[int]:
+        """
+        If day is not provided, return the water consumption in liter for each hour for
+        the given day. The first item matches 1h, the last one 24h.
+        Else return the return the water consumption in liter for each day for the
+        given month.
+        The consumption is not available for the last 3 days.
+        """
         if year < 2001:
             raise ValueError("year must be greater than 2000")
 
+        if date(year, month, day if day else 1) > datetime.now().date():
+            raise ValueError("Cannot retrieve consumption from the future.")
+
+        if day:
+            return await self._get_hourly_consumption(month, year, day)
+        return await self._get_daily_consumption(month, year)
+
+    async def _get_daily_consumption(self, month: int, year: int) -> List[int]:
         async with self.session.get(DATA_URL.format(month, year)) as response:
             if response.url.name == "inscription.aspx":
                 raise NotAuthenticatedException
             data = await response.text()
 
-        reader = csv.reader(data.splitlines(), delimiter=";")
-        next(reader)  # skip header line
-        return {
-            datetime.strptime(row[0], "%d/%m/%Y").timestamp(): int(row[1])
-            for row in reader
-        }
+        reader = csv.DictReader(data.splitlines(), delimiter=CSV_DELIMITER)
+        return [int(row[CONSUMPTION_HEADER]) for row in reader]
+
+    async def _get_hourly_consumption(
+        self, month: int, year: int, day: int
+    ) -> List[int]:
+        if date(year, month, day) > self.last_report_date:
+            raise ValueError(
+                f"Hourly consumption is only available for date "
+                f"before {self.last_report_date}"
+            )
+
+        async with self.session.get(DATA_URL_DAY.format(month, year, day)) as response:
+            if response.url.name == "inscription.aspx":
+                raise NotAuthenticatedException
+            data = await response.text()
+
+        reader = csv.DictReader(data.splitlines(), delimiter=CSV_DELIMITER)
+        try:
+            return [int(row[CONSUMPTION_HEADER]) for row in reader]
+        except IndexError:
+            # Hourly consumption is not enabled
+            return []
 
     async def login(self) -> None:
         """Log into the Veolia website."""
