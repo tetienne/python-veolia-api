2dea5be837002172e78eb72eb48956b818cc2aed
ghost58400 <jojo58400@hotmail.fr>
2022-07-18 12:40:10 +0200

    Update veolia_websites.py

--- pyolia/veolia_websites.py
+++ pyolia/veolia_websites.py
@@ -5,5 +5,5 @@ from enum import Enum, unique
 
 @unique
 class VeoliaWebsite(str, Enum):
-    EAU_SERVICES = "eau-services.com"
+    EAU_SERVICES = "www.eau-services.com"
     EAU_DU_GRAND_LYON = "agence.eaudugrandlyon.com"
